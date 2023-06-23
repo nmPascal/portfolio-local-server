beed1f2740dd9c1199f0c313e3332d3d3dc074db
PascalHcr <pascal.hector@netmedia.de>
2023-06-23 11:39:15 +0200

    change project data structure

--- src/graphql/types/projectType.ts
+++ src/graphql/types/projectType.ts
@@ -1,12 +1,14 @@
-import { GraphQLID, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
 
 export const ProjectType = new GraphQLObjectType({
     name: "Project",
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        technologie: { type: GraphQLString },
-        source: { type: GraphQLString },
+        primary_language: { type: GraphQLString },
+        technologies: { type: new GraphQLList(GraphQLString) },
+        source_code: { type: GraphQLString || null },
+        live_demo: { type: GraphQLString || null },
         description: { type: GraphQLString },
         image: { type: GraphQLString },
     }),
